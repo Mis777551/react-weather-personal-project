337ca41d976a12d8ab99038e4a30791cc5fcaa05
vidimamisokuhle <vidimamisokuhle@gmail.com>
2025-07-03 16:55:48 +0200

    linked axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import axios from "axios";
 
 function App() {
+
+  const url= `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`
   return (
     <div className="App">
       <h1>Hello</h1>
