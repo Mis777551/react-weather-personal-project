85fe293a6d1bfc03645b64ccf23d24d2806cecd4
vidimamisokuhle <vidimamisokuhle@gmail.com>
2025-07-03 17:14:10 +0200

    created html structure

--- src/App.js
+++ src/App.js
@@ -5,8 +5,35 @@ function App() {
 
   const url= `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <div className="container"></div>
+      <div className="top">
+        <div className="location">
+          <p>Dallas</p>
+        </div>
+        <div className="temp">
+          <h1>60 F</h1>
+        </div>
+        <div className="description">
+          <p>clouds</p>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="feels">
+          65 F
+        </div>
+        <div className="humidity">
+          20%
+        </div>
+        <div className="wind">
+          12MPH
+        </div>
+
+
+      </div>
+
+
+      
     </div>
   );
 }
